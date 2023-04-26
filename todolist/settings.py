61507b13bd2ac62c5bcfe7c40f0be651ed1b8366
sergd <videomc1979@gmail.com>
2023-04-26 20:56:06 +0300

    CW_7 Added migrations and tested the application

--- todolist/settings.py
+++ todolist/settings.py
@@ -42,15 +42,15 @@ ALLOWED_HOSTS = ["*"]
 
 # Application definition
 INSTALLED_APPS = [
-    # ===== My Apps =====
-    'core.apps.CoreConfig',
-    # ====================
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # ===== My Apps =====
+    'core.apps.CoreConfig',
+    # ====================
 ]
 
 MIDDLEWARE = [
