e2d044f65c4ecf02198b5094a46c91841fce11d7
sergd <videomc1979@gmail.com>
2023-04-30 17:24:33 +0300

    CW_7 changed settings

--- todolist/settings.py
+++ todolist/settings.py
@@ -84,23 +84,23 @@ WSGI_APPLICATION = 'todolist.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-# ==== ПОДКЛЮЧЕНИЕ К БАЗЕ ДАННЫХ PostgreSQL ====
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.environ.get('SQL_DB_NAME'),
-#         'USER': os.environ.get('SQL_DB_USER'),
-#         'PASSWORD': os.environ.get('SQL_DB_PASSWORD'),
-#         'HOST': os.environ.get('SQL_DB_HOST'),
-#         'PORT': os.environ.get('SQL_DB_PORT'),
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+# ==== ПОДКЛЮЧЕНИЕ К БАЗЕ ДАННЫХ PostgreSQL ====
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('SQL_DB_NAME'),
+        'USER': os.environ.get('SQL_DB_USER'),
+        'PASSWORD': os.environ.get('SQL_DB_PASSWORD'),
+        'HOST': os.environ.get('SQL_DB_HOST'),
+        'PORT': os.environ.get('SQL_DB_PORT'),
+    }
+}
 
 
 # Password validation
