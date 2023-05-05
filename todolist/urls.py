69600da6d68db1896e209a08609a28afb4afe383
sergd <videomc1979@gmail.com>
2023-05-05 14:23:16 +0300

    CW_7 поправил urls

--- todolist/urls.py
+++ todolist/urls.py
@@ -20,8 +20,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('social-auth/', include('social_django.urls', namespace="social")),
-    path('api/', include('core.urls')),
+    path('oauth/', include('social_django.urls', namespace="social")),
+    path('core/', include('core.urls')),
 ]
 
 if settings.DEBUG:
