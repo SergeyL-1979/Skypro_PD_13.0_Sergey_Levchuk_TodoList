5b45c2e458fee6f438210e25129d9d11f0a3862c
sergd <videomc1979@gmail.com>
2023-05-06 20:38:29 +0300

    CW_7 поправил v7

--- todolist/settings.py
+++ todolist/settings.py
@@ -66,10 +66,6 @@ MIDDLEWARE = [
 
 # ==== Настройки CORS ====================
 CORS_ALLOW_ALL_ORIGINS = True
-# CORS_ALLOWED_ORIGINS = [
-#     "http://localhost:8000",
-#     "http://51.250.67.65/",
-# ]
 
 CSRF_TRUSTED_ORIGINS = [
     'http://51.250.67.65',
