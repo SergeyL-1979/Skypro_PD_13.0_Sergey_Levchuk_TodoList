74fcebf981359db00a19a6a35d2cd027b07fed08
sergd <videomc1979@gmail.com>
2023-05-06 18:28:34 +0300

    CW_7 поправил CORS v6

--- todolist/settings.py
+++ todolist/settings.py
@@ -71,7 +71,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 #     "http://51.250.67.65/",
 # ]
 
-CSRF_TRUSTED_ORIGINS = ['http://51.250.67.65/']
+CSRF_TRUSTED_ORIGINS = True
 
 ROOT_URLCONF = 'todolist.urls'
 
