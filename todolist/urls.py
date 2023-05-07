36b643014778742b2b2f33f72b31163fa3558943
sergd <videomc1979@gmail.com>
2023-05-07 18:14:14 +0300

    CW_7 API v2

--- todolist/urls.py
+++ todolist/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/oauth/', include('social_django.urls', namespace="social")),
+    path('oauth/', include('social_django.urls', namespace="social")),
     path('core/', include('core.urls')),
 ]
 
