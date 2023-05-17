8fc066df85be2e560d0a4b15c44fe2810251fd2a
sergd <videomc1979@gmail.com>
2023-05-17 17:00:15 +0300

    CW_7 проверка суперпользователя

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ python manage.py migrate --check
 status=$?
 if [[ status != 0 ]]; then
   python manage.py migrate
-  python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('${SUPERUSER_USERNAME}', '${SUPERUSER_EMAIL}', '${SUPERUSER_PASSWORD}')"
+  python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('entrypoint', 'entry@mail.ru', '123456Bolid')"
 
 fi
 python manage.py collectstatic --no-input --clear
