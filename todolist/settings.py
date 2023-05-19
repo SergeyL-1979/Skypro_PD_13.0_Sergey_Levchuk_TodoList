0c5f4b53c772b19028166fb0f6a45d1b3b2bdaa1
sergd <videomc1979@gmail.com>
2023-05-19 11:51:06 +0300

    CW_7 lesson_37 DEFAULT_FILTER_BACKENDS

--- todolist/settings.py
+++ todolist/settings.py
@@ -236,6 +236,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
     ],
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend', ],
 }
 
 # SPECTACULAR_SETTINGS = {
