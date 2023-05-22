e4d2f48dcae0ecb9a573990dd07a6e8635292b33
sergd <videomc1979@gmail.com>
2023-05-22 15:04:56 +0300

    CW_7 lesson_37 исправил

--- goals/models.py
+++ goals/models.py
@@ -5,9 +5,6 @@ from core.models import User
 
 
 class DatesModelMixin(models.Model):
-    class Meta:
-        abstract = True
-
     created = models.DateTimeField(verbose_name="Дата создания")
     updated = models.DateTimeField(verbose_name="Дата последнего обновления")
 
@@ -16,6 +13,9 @@ class DatesModelMixin(models.Model):
             self.created = timezone.now()
         self.updated = timezone.now()
         return super().save(*args, **kwargs)
+
+    class Meta:
+        abstract = True
 
 
 class GoalCategory(DatesModelMixin):
