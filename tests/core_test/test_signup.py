3cdb6aef73ab7610a0eb7d1554a1fdf7c9e832bc
sergd <videomc1979@gmail.com>
2023-06-09 10:50:44 +0300

    CW_7 тест signup correct

--- tests/core_test/test_signup.py
+++ tests/core_test/test_signup.py
@@ -12,6 +12,7 @@ class TestSignUpView:
     url = reverse('core:signup')
 
     def test_user_created(self, client):
+        # data = SignUpRequest.build()
         data = {
             'username': 'test',
             'first_name': 'test',
@@ -20,10 +21,12 @@ class TestSignUpView:
             'password': 'test12234567',
             'password_repeat': 'test12234567'
         }
+
         response = client.post(self.url, data=data)
 
         assert response.status_code == status.HTTP_201_CREATED
         user = User.objects.get()
+        assert response.json() == self._serialize_response(user)
         assert user.check_password(data['password'])
 
     @pytest.mark.parametrize(
@@ -51,32 +54,33 @@ class TestSignUpView:
         assert response.status_code == status.HTTP_400_BAD_REQUEST
         # assert response.json() == {'non_field_errors': ['Passwords must match']}
 
-# ===== ДАННЫЙ МЕТОД ТЕСТИРОВАНИЯ НЕ ПРОХОДИТ  ========================
-# def test_user_already_exists(self, client, user):
-#     # data = {
-#     #     'username': 'test',
-#     #     'first_name': 'test',
-#     #     'last_name': 'test',
-#     #     'email': 'test@test.ru',
-#     #     'password': 'test12234567',
-#     #     'password_repeat': 'test12234567'
-#     # }
-#
-#     data = SignUpRequest.build(username=user.username)
-#
-#     response = client.post(self.url, data=data)
-#
-#     assert response.status_code == status.HTTP_400_BAD_REQUEST
-#     assert response.json() == {'username': ['A user with that username already exists.']}
+    @staticmethod
+    def _serialize_response(user: User, **kwargs) -> dict:
+        data = {
+            'id': user.id,
+            'username': user.username,
+            'first_name': user.first_name,
+            'last_name': user.last_name,
+            'email': user.email,
+        }
+        data |= kwargs
+        return data
 
-# @staticmethod
-# def _serialize_response(user: User, **kwargs) -> dict:
-#     data = {
-#         'id': user.id,
-#         'username': user.username,
-#         'first_name': user.first_name,
-#         'last_name': user.last_name,
-#         'email': user.last_name,
-#     }
-#     data |= kwargs
-#     return data
+
+# ===== ДАННЫЙ МЕТОД ТЕСТИРОВАНИЯ НЕ ПРОХОДИТ  ========================
+#     def test_passwords_missmatch(self, client, faker):
+#         data = SignUpRequest.build(password_repeat=faker.password())
+#         response = client.post(self.url, data=data)
+#
+#         assert response.status_code == status.HTTP_400_BAD_REQUEST
+#         assert response.json() == {'non_field_errors': ['Passwords must match']}
+#
+#
+#     def test_user_already_exists(self, client, user):
+#         data = SignUpRequest.build(username=user.username)
+#
+#         response = client.post(self.url, data=data)
+#
+#         assert response.status_code == status.HTTP_400_BAD_REQUEST
+#         assert response.json() == {'username': ['A user with that username already exists.']}
+
