2416210c93a146424cd0c23cf589226b2c5e6842
sergd <videomc1979@gmail.com>
2023-06-10 14:00:42 +0300

    CW_7 correct test_bot.py

--- tests/bot_test/test_bot.py
+++ tests/bot_test/test_bot.py
@@ -1,38 +1,83 @@
-# import json
-# import pytest
-# from django.urls import reverse
-# from rest_framework import status
-# from tests import factories
-#
-#
+from unittest.mock import patch
+
+import pytest
+from django.urls import reverse
+from rest_framework import status
+
+from bot.tg.client import TgClient
+from core.models import User
+from tests import factories
+
+
+@pytest.mark.django_db
+class TestTgUser:
+    url: str = reverse('bot:verify')
+
+    def test_bot_verify(self, auth_client, user: User):
+        tg_user = factories.TuserFactory.create(
+            chat_id='124315315',
+            user_ud='124315315',
+            username='test_user',
+            user=user,
+            verification_code='correct'
+        )
+        payload = {'verification_code': 'correct'}  # A A A
+
+        with patch.object(TgClient, "send_message") as mock:
+            response = auth_client.patch(self.url, data=payload)
+
+        tg_user.refresh_from_db()
+        assert tg_user.user == user
+        assert response.status_code == status.HTTP_200_OK
+        mock.assert_called_once_with(tg_user.chat_id, '[verification has been completed]')
+
+    def test_incorrect(self, auth_client, client, user: User):
+        tg_user = factories.TuserFactory.create(
+            chat_id='124315315',
+            user_ud='124315315',
+            username='test_user',
+            user=user,
+            verification_code='correct'
+        )
+        payload = {'verification_code': 'incorrect'}
+
+        with patch.object(TgClient, "send_message") as mock:
+            response = auth_client.patch(self.url, data=payload)
+            mock.assert_not_called()
+
+        tg_user.refresh_from_db()
+        assert tg_user.user == user
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        mock.assert_not_called()
+
+# ================= ОРИГИНАЛ =====================
 # @pytest.mark.django_db
-# def test_bot_verify(auth_client, user):
+# def test_bot_verify(auth_client, user: TgUser):
 #     factories.TuserFactory.create(
-#         chat_id=124315315,
-#         user_id=12431,
-#         user_ud=124315315,
+#         chat_id='124315315',
+#         user_ud='124315315',
 #         username='test_user',
 #         user=user,
 #         verification_code='correct'
 #     )
 #     url: str = reverse('bot:verify')
-#     payload1 = {
-#         'verification_code': 'correct'
-#     }
-#     payload2 = {
-#         'verification_code': 'incorrect'
-#     }
+#     payload1 = {'verification_code': 'correct'}
+#     payload2 = {'verification_code': 'incorrect'}
 #
-#     response1 = auth_client.patch(
-#         path=url,
-#         data=json.dump(payload1),
-#         content_type='application/json',
-#     )
-#     response2 = auth_client.patch(
-#         path=url,
-#         data=json.dumps(payload2),
-#         content_type='application/json',
-#     )
+#     with patch.object(TgClient, "send_message") as mock:
 #
-#     assert response1.status_code == status.HTTP_201_CREATED
+#         response1 = auth_client.patch(
+#             path=url,
+#             data=payload1,
+#         )
+#         mock.assert_called_once_with(124315315, '[verification has been completed]')
+#
+#     with patch.object(TgClient, "send_message") as mock:
+#         response2 = auth_client.patch(
+#             path=url,
+#             data=payload2,
+#         )
+#         mock.assert_not_called()
+#
+#     assert response1.status_code == status.HTTP_200_OK
 #     assert response2.status_code == status.HTTP_400_BAD_REQUEST
